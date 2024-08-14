67fc475eee3def7c02e348afc59ead2ce50d6d5e
Benoit <you@example.com>
2024-08-14 08:56:50 -0400

    bot 1.2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const { Client, IntentsBitField } = require('discord.js');
+const eventHandler = require('./handlers/eventHandler');
 
 const client = new Client({
   intents: [
@@ -10,36 +11,6 @@ const client = new Client({
   ],
 });
 
-client.on('ready', (c) => {
-  console.log(`✅ ${c.user.tag} is online.`);
-});
-
-client.on(`interactionCreate`, async (interaction) => {
-  try {
-    if (!interaction.isButton()) return;
-  await interaction.deferReply({ephemeral: true});
-  const role = interaction.guild.roles.cache.get(interaction.customId);
-  if (!role) {
-    interaction.editReply({
-      content: "I couldn't find that role",
-    })
-    return;
-}
-
-const hasRole = interaction.member.roles.cache.has(role.id);
-
-if (hasRole) {
-  await interaction.member.roles.remove(role);
-  await interaction.editReply(`The role ${role} has been removed`);
-  return;
-}
-
-await interaction.member.roles.add(role);
-await interaction.editReply(`The role ${role} has been added.`);
-
-  } catch (error) {
-    console.log(error);
-  }
-})
+eventHandler(client);
 
 client.login(process.env.TOKEN);
